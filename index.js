a046b167271b9f8d7fc730258d1802c4dcca01b9
Gaurav parmar <54779403+thegauravparmar@users.noreply.github.com>
2021-06-14 22:55:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,4 +33,4 @@ io.on("connection", (socket) => {
 	});
 });
 
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
